40e9f10e7be2fcf9a179272a43689f8ccccaadc5
yash-purwar2004 <yashpurwar2004@gmail.com>
2026-02-10 23:16:22 +0530

    Initialize SpringBoot Project

---
+++ conversoBackend/src/test/java/com/example/conversoBackend/ConversoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conversoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
